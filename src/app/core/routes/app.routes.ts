47b81fc02865ae7c9c018247db00f439b4df6869
vmarquezv <vmarquezv@debian.vmarquezv>
2025-04-08 13:02:48 -0300

    [config]: Add adm routes

--- src/app/core/routes/app.routes.ts
+++ src/app/core/routes/app.routes.ts
@@ -5,10 +5,8 @@ import { ProductPageComponent } from '../../pages/product-page/product-page.comp
 import { CartPageComponent } from '../../pages/cart-page/cart-page.component';
 import { PaymentPageComponent } from '../../pages/payment-page/payment-page.component';
 import { OrderComponent } from '../../pages/order/order.component';
-import { DashboardComponent } from '../../admin/dashboard/dashboard.component'; // Ensure this is correct
-import { AdminComponent } from '../../admin/admin.component'; // Import the admin components
-import { ADMIN_ROUTES } from './admin.routes'; // Import the admin routes
 import { adminGuard } from '../guards/admin.guard';
+import { AdminComponent } from '../../admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,10 +15,8 @@ export const routes: Routes = [
   { path: 'cart', component: CartPageComponent },
   { path: 'payment', component: PaymentPageComponent },
   { path: 'order', component: OrderComponent },
-  // Add the admin route with lazy loading
+
   {
-    path: 'admin',
-    loadChildren: () => import('../routes/admin.routes').then(m => m.ADMIN_ROUTES),
-    canMatch: [adminGuard]
+    path: 'admin', component: AdminComponent
   },
 ];
